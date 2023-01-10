ac83afb022c37f425f7ae70851d240f9daba8eee
Mingfei Cui <mingfey.zui@gmail.com>
2023-01-10 15:07:30 +0100

    Some basic rules of Shengji

---
+++ src/main.py
@@ -0,0 +1,11 @@
+
+# rules:
+# Nummer of cards: 54
+# for each 1 mals: ColorJocker > BWJoker >
+# for each 4 mals: 2 > 1 > K > Q > J > 10 >9 > 8 > 7 > 6 > 5 > 4 > 3 > 1
+# players: 4, 2 teams
+# playing round card: 3-4-5-6-7-8-9-10-J-Q-K
+# playing round changing: 1. which one has the higer cards; 2 heritate from team member, who gets rid of the whole hand cards
+# end of playing round: which team first get rid of all cards
+# end of play: 1. short game: which team with higer number in playing round;  2. long game: which team first reach K
+# single card , double card , triple card , quartor card
